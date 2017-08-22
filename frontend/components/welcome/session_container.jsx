3f0e747801fde987808a07bc121523e8f2af694e
anthony rondinone <a.rondinone@gmail.com>
2017-08-22 12:29:13 -0400

    Finish session container

---
+++ frontend/components/welcome/session_container.jsx
@@ -0,0 +1,26 @@
+import { connect } from 'react-redux';
+import SessionForm from './session_form';
+import {login, signup} from '../../actions/session_actions';
+
+const mapStateToProps = (state, ownProps) => {
+  // debugger
+  return {
+    loggedIn: Boolean(state.session.currentUser),
+    errors: state.session.errors,
+    formType: ownProps.location.pathname
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  if (ownProps.location.pathname === '/signin') {
+    return {
+      processForm: (user) => dispatch(login(user))
+    };
+  } else if (ownProps.location.pathname === '/signup') {
+    return {
+      processForm: (user) => dispatch(signup(user))
+    };
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SessionForm);
