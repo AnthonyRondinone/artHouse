f356d063ce95af4933c8098b614fb9a925b7388d
Anthony Rondinone <a.rondinone@gmail.com>
2017-10-26 17:20:36 -0400

    Take funcs off window

--- frontend/art_house.jsx
+++ frontend/art_house.jsx
@@ -3,26 +3,12 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-//
-import { sendBid } from './util/bid_api_util';
-import { RECEIVE_BID, addBid, receiveBid } from './actions/bid_actions';
-import {addNewLike} from './actions/like_actions';
-//
-
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   let store = configureStore();
 
-  // test
-  window.sendBid = sendBid;
-  window.addBid = addBid;
-  window.dispatch = store.dispatch;
-  window.getState = store.getState;
-  window.addNewLike = addNewLike;
-
-  //
 
   if (window.currentUser) {
     const preloadedState = { session: { currentUser: window.currentUser, errors: [] } };
